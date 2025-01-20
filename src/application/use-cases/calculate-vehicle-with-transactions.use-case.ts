54700d872130df2061e60f01b4d8d0ff328b87e9
Léo Turpin <turpin_l@etna-alternance.net>
2025-01-20 14:33:00 +0100

    feat: finish calculate vehicle use case

--- src/application/use-cases/calculate-vehicle-with-transactions.use-case.ts
+++ src/application/use-cases/calculate-vehicle-with-transactions.use-case.ts
@@ -1,7 +1,61 @@
-import type { Vehicle, VehicleTransaction } from '@zcorp/shared-typing-wheelz';
+import type {
+  UpdateVehicleTransactionData,
+  Vehicle,
+  VehicleTransaction,
+} from '@zcorp/shared-typing-wheelz';
 
 export class CalculateVehicleWithTransactionsUseCase {
   execute(vin: string, transactions: VehicleTransaction[]): Vehicle | null {
-    return null;
+    const concernedTransactions = transactions.filter((transaction) => {
+      return transaction.data.vin === vin;
+    });
+    if (concernedTransactions.length === 0) {
+      return null;
+    }
+    let vehicle: Vehicle | null = null;
+    for (const transaction of concernedTransactions) {
+      switch (transaction.action) {
+        case 'create': {
+          vehicle = transaction.data;
+          break;
+        }
+        case 'update': {
+          if (vehicle) {
+            vehicle = this._updateVehicle(vehicle, transaction.data);
+          }
+          break;
+        }
+        case 'delete': {
+          vehicle = null;
+          break;
+        }
+      }
+    }
+    return vehicle;
+  }
+  _updateVehicle(vehicle: Vehicle, transaction: UpdateVehicleTransactionData): Vehicle {
+    const featuresChanges = transaction.changes.features;
+    const infosChanges = transaction.changes.infos;
+    const technicalControlsChanges = transaction.changes.technicalControls;
+    const historyChanges = transaction.changes.history;
+    const sinisterInfosChanges = transaction.changes.sinisterInfos;
+
+    return {
+      vin: vehicle.vin,
+      features: {
+        ...vehicle.features,
+        ...featuresChanges,
+      },
+      infos: {
+        ...vehicle.infos,
+        ...infosChanges,
+      },
+      technicalControls: technicalControlsChanges ?? vehicle.technicalControls,
+      history: historyChanges ?? vehicle.history,
+      sinisterInfos: {
+        ...vehicle.sinisterInfos,
+        ...sinisterInfosChanges,
+      },
+    };
   }
 }
